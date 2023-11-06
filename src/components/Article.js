8bca18c83976f08558058ad4094f1bef33c5ff94
Yumie Tsuzuki <yumie.t.29@gmail.com>
2023-11-06 11:42:13 +0900

    20231105-redesign news page

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,7 @@
 import { formatDate } from "../utils/formatDate";
 import { truncateString } from "../utils/truncateString";
 
-function Article({ news, tempImage, btnColor }) {
+function Article({ news, tempImage }) {
   const { title, description, image_url, link, creator, pubDate } = news;
 
   return (
@@ -28,7 +28,7 @@ function Article({ news, tempImage, btnColor }) {
 
       <div className="mt-11 flex justify-end">
         <a
-          className={`bg-white-secondary px-3.5 py-2 text-white-primary rounded text-sm text-right hover:${btnColor} hover:text-black`}
+          className="bg-gray-100 px-3.5 py-2 text-white rounded text-sm text-right hover:bg-green-400"
           href={link}
           target="_blank"
           rel="noreferrer"
--- src/pages/News.js
+++ src/pages/News.js
@@ -23,11 +23,11 @@ function News() {
   // }, []);
 
   return (
-    <div className="px-12 py-9 bg-background-primary row-span-2 h-max">
+    <div className="w-full">
       <h1 className="text-4xl font-bold mb-3 text-center">
         The latest tech news
       </h1>
-      <p className="text-lg text-center mb-16">
+      <p className="text-lg text-center mb-14">
         Learn how to grow your business with our expert advice.
       </p>
 
