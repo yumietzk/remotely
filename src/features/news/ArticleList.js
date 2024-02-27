2ae8d0a5385764a0c9c5d5726a2e5ff88bc996ac
Yumie Tsuzuki <yumie.t.29@gmail.com>
2024-02-26 20:50:24 -0500

    20240226-remove the duplicated news data and update top page

--- src/features/news/ArticleList.js
+++ src/features/news/ArticleList.js
@@ -2,11 +2,21 @@ import ArticleCard from "./ArticleCard";
 import { placeholderImg } from "../../data/placeholderImg";
 
 function ArticleList({ news }) {
+  // Remove the duplicated news
+  let newData = [];
+  let title = [];
+  news.forEach((item) => {
+    if (!title.includes(item.title)) {
+      newData.push(item);
+      title.push(item.title);
+    }
+  });
+
   return (
     <div className="grid grid-cols-3 gap-x-14 gap-y-20">
-      {news.map((item, i) => (
+      {newData.map((item, i) => (
         <ArticleCard
-          key={item.title}
+          key={item.article_id}
           news={item}
           tempImage={placeholderImg[i]}
         />
--- src/pages/Toppage.js
+++ src/pages/Toppage.js
@@ -6,25 +6,31 @@ function TopPage() {
   const { user } = useUser();
 
   return (
-    <div className="h-screen w-full font-primary font-normal text-base bg-green-500 text-white px-12 py-7">
-      <header className="mb-14">
+    <div className="h-screen w-full flex flex-col font-primary font-normal text-base bg-green-500 text-white px-12 py-10">
+      <header className="h-9 mb-24">
         <div className="text-3xl font-medium font-secondary">Remotely 🌎</div>
       </header>
 
-      <h1 className="text-5xl font-bold mb-3">Find Your New Remote Job</h1>
-      <p className="text-2xl mb-16">
-        We help you find your new Software Engineer job available from where you
-        are located.
-      </p>
-      <Link
-        to={user ? "/dashboard" : "/signin"}
-        className="px-5 py-3.5 rounded border border-white text-2xl transition-colors duration-300 hover:bg-white h hover:text-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent focus:ring-offset-green-500"
-      >
-        Get started!
-      </Link>
+      <div className="flex-1 flex items-center">
+        <div className="flex-1 pb-16">
+          <h1 className="text-6xl font-bold tracking-wide leading-normal mb-10">
+            Find Your New Remote Job
+          </h1>
+          <p className="text-2xl tracking-wide mb-40">
+            We help you find your new Software Engineer job available from where
+            you are located.
+          </p>
+          <Link
+            to={user ? "/dashboard" : "/signin"}
+            className="px-5 py-3.5 rounded border border-white text-2xl transition-colors duration-300 hover:bg-white h hover:text-green-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-accent focus:ring-offset-green-500"
+          >
+            Get started!
+          </Link>
+        </div>
 
-      <div className="absolute bottom-0 right-32 flex justify-end">
-        <img className="h-[500px] w-auto" src={image} alt="toppage" />
+        <div className="w-[700px] h-auto ml-11">
+          <img className="object-cover" src={image} alt="toppage" />
+        </div>
       </div>
     </div>
   );
