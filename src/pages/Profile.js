be1c4b240762d6214ec7d9cfebdca2416cdbb0f7
Yumie Tsuzuki <yumie.t.29@gmail.com>
2024-03-08 16:21:04 -0500

    20240308-minor fix

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,7 +5,7 @@ function Profile() {
   const [profile, getProfile] = useOutletContext();
 
   return (
-    <div className="overflow-y-scroll">
+    <div className="h-full overflow-y-scroll">
       <div className="bg-white w-9/12 mx-auto py-7 px-8 lg:py-9 lg:px-10 rounded-lg">
         <div className="flex justify-between items-end mb-8">
           <h2 className="text-base lg:text-xl font-semibold">
